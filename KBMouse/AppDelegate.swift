ef8cf1e2b0a8a39811236675eaec55a74be7bc8d
kayama <kayama@kayama-pc.local>
2015-09-10 23:15:56 +0900

    Initial Commit

---
+++ KBMouse/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KBMouse
+//
+//  Created by kayama on 2015/09/10.
+//  Copyright (c) 2015年 kayama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KBMouseTests/KBMouseTests.swift
@@ -0,0 +1,36 @@
+//
+//  KBMouseTests.swift
+//  KBMouseTests
+//
+//  Created by kayama on 2015/09/10.
+//  Copyright (c) 2015年 kayama. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class KBMouseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
