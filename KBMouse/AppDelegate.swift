9bb21db6eb4e40733da99031834b07628ced2753
kayama <kayama@kayama-pc.local>
2015-09-10 23:54:43 +0900

    Small fix

--- KBMouse/AppDelegate.swift
+++ KBMouse/AppDelegate.swift
@@ -11,13 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+   // @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+ 
     }
-
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
